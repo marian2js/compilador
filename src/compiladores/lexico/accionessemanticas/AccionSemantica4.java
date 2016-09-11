1c12da02810e4311431e60a1394f337f024d1353
luksbelt <lucaskpomdq@gmail.com>
2016-09-11 02:31:48 -0300

    Add Accion Semantica 4 Error de caracter

---
+++ src/compiladores/lexico/accionessemanticas/AccionSemantica4.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compiladores.lexico.accionessemanticas;
+
+import compiladores.Token;
+import compiladores.Error;
+import compiladores.Logger;
+import compiladores.lexico.AnalizadorLexico;
+
+/**
+ *
+ * @author luksbelt
+ */
+public class AccionSemantica4 extends AccionSemantica{
+    //Error de caracter
+    @Override
+    public Token ejecutar(AnalizadorLexico analizadorLexico, char c) {
+        int linea = analizadorLexico.getLinea();
+        Error e = new Error("Error de caracter invalido",linea,"Lexico");
+        Logger log = Logger.getLog();
+        log.addError(e);
+        analizadorLexico.consumir();
+        analizadorLexico.setBuffer("");
+        return null;
+    }
+    
+}
