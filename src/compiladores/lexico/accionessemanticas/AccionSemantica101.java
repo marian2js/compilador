8f5cf480e1c46c6d1b14f6c9ec7151421acc305a
marian2js <marianomdq1@gmail.com>
2016-09-10 23:13:39 -0300

    Add float AS instance

---
+++ src/compiladores/lexico/accionessemanticas/AccionSemantica101.java
@@ -0,0 +1,13 @@
+package compiladores.lexico.accionessemanticas;
+
+import compiladores.Token;
+import compiladores.lexico.AnalizadorLexico;
+
+public class AccionSemantica101 extends AccionSemantica {
+    // Floats
+    
+    @Override
+    public Token ejecutar(AnalizadorLexico analizadorLexico, char c) {
+        return null;
+    }
+}
