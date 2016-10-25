59e688dff11dd0af7688f7707bd2c866dec0fed0
marian2js <marianomdq1@gmail.com>
2016-10-25 17:15:20 -0300

    Agrega constructor de Tercetos

--- src/compiladores/Terceto.java
+++ src/compiladores/Terceto.java
@@ -1,15 +1,22 @@
 package compiladores;
 
 public class Terceto extends Objeto {
-    public String getOperando() {
-        return (String)this.atributos.get("operando");
+
+    public Terceto(String operacion, Objeto operando1, Objeto operando2) {
+        this.atributos.put("operacion", operacion);
+        this.atributos.put("operando1", operando1);
+        this.atributos.put("operando2", operando2);
     }
 
-    public Objeto getOperacion1() {
-        return (Objeto)this.atributos.get("operacion1");
+    public String getOperacion() {
+        return (String)this.atributos.get("operacion");
     }
 
-    public Objeto getOperacion2() {
-        return (Objeto)this.atributos.get("operacion2");
+    public Objeto getOperando1() {
+        return (Objeto)this.atributos.get("operando1");
+    }
+
+    public Objeto getOperando2() {
+        return (Objeto)this.atributos.get("operando2");
     }
 }
