6f949bafd81d4f5d1c82bf6f0d91a7baff2596c2
Gonzalo Sassoni <gonzasso@gmail.com>
2016-11-10 19:07:20 -0300

    Agregada generacion de codigo assembler para el terceto asignacion

--- src/compiladores/tercetos/TercetoAsignacion.java
+++ src/compiladores/tercetos/TercetoAsignacion.java
@@ -6,4 +6,11 @@ public class TercetoAsignacion extends Terceto {
     public TercetoAsignacion(Objeto operando1, Objeto operando2) {
         super(":=", operando1, operando2);
     }
+
+    @Override
+    public String getAssembler() {
+        String codigo = "MOV BX, " + this.getValor() + "\n" +
+                        "MOV " + getOperando1().getValor() + ", BX\n";
+        return codigo;
+    }
 }
