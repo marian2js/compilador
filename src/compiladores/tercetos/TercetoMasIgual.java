71c231348aa9ad820542d51227af4957a9c5ba40
marian2js <marianomdq1@gmail.com>
2016-11-15 19:00:19 -0300

    Implementa terceto mas igual

---
+++ src/compiladores/tercetos/TercetoMasIgual.java
@@ -0,0 +1,19 @@
+package compiladores.tercetos;
+
+import compiladores.Objeto;
+
+public class TercetoMasIgual extends Terceto {
+    public TercetoMasIgual(Objeto operando1, Objeto operando2) {
+        super(":=", operando1, operando2);
+    }
+
+    @Override
+    public String getAssembler() {
+        if (getOperando1().getTipo().equals("integer")) {
+            return "MOV BX, " + getOperando2().getValor() + "\n" +
+                   "ADD BX, " + getOperando1().getValor() + "\n" +
+                   "MOV " + getOperando1().getValor() + ", BX\n";
+        }
+
+    }
+}
