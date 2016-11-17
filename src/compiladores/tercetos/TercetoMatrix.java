c795bce20b8f8af917b0c79e9af4d69f734bb443
marian2js <marianomdq1@gmail.com>
2016-11-17 02:53:03 -0300

    Intentando guardar indices...

--- src/compiladores/tercetos/TercetoMatrix.java
+++ src/compiladores/tercetos/TercetoMatrix.java
@@ -5,22 +5,27 @@ import compiladores.Objeto;
 import compiladores.Token;
 
 public class TercetoMatrix extends Terceto {
+    public static int indexId = 0;
     private Double offset;
+    private int index;
 
     public TercetoMatrix(Objeto operando1, Double offset) {
         super("matrix", operando1, null);
         this.offset = offset;
+        index = indexId;
+        indexId++;
     }
 
     @Override
     public String getAssembler() {
         return "MOV ECX, OFFSET " + getOperando1().getValor() + "\n" +
-               "ADD CX, " + offset.toString().replace(".0", "") + "\n";
+               "ADD CX, " + offset.toString().replace(".0", "") + "\n" +
+               "MOV @index" + index + ", ECX\n";
     }
 
     @Override
     public String getValor() {
-        return "sword ptr[ECX]";
+        return "sword ptr[@index" + index + "]";
     }
 
     @Override
